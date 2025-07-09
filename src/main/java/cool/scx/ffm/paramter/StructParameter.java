ae5a12c638a87d9c3538c0a571c460cba39a5ab5
scx567888 <scx567888@outlook.com>
2025-07-09 15:17:17 +0800

    dev

--- src/main/java/cool/scx/ffm/paramter/StructParameter.java
+++ src/main/java/cool/scx/ffm/paramter/StructParameter.java
@@ -1,6 +1,10 @@
 package cool.scx.ffm.paramter;
 
 import cool.scx.ffm.struct.Struct;
+import cool.scx.reflect.AccessModifier;
+import cool.scx.reflect.ClassInfo;
+import cool.scx.reflect.FieldInfo;
+import cool.scx.reflect.ScxReflect;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemoryLayout;
@@ -8,7 +12,7 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.StructLayout;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
-import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,30 +26,30 @@ import static cool.scx.ffm.FFMHelper.getMemoryLayout;
 public class StructParameter implements Parameter {
 
     private final Object value;
-    private final Map<Field, VarHandle> fieldMap;
+    private final Map<FieldInfo, VarHandle> fieldMap;
     private final StructLayout LAYOUT;
     private MemorySegment memorySegment;
 
     public StructParameter(Struct value) {
         this.value = value;
-        var classInfo = this.value.getClass();
+        var classInfo = (ClassInfo) ScxReflect.getType(this.value.getClass());
         this.fieldMap = new HashMap<>();
         //1, 寻找 public 的 fields
-        var fields = classInfo.getFields();
+        var fields = Arrays.stream(classInfo.allFields()).filter(c -> c.accessModifier() == AccessModifier.PUBLIC && !c.isStatic()).toList();
 
         //2, 创建每个 field 对应的 内存布局
-        var memoryLayouts = new MemoryLayout[fields.length];
-        for (int i = 0; i < fields.length; i = i + 1) {
-            var f = fields[i];
-            var memoryLayout = getMemoryLayout(f.getType());
-            memoryLayouts[i] = memoryLayout.withName(f.getName());
+        var memoryLayouts = new MemoryLayout[fields.size()];
+        for (int i = 0; i < fields.size(); i = i + 1) {
+            var f = fields.get(i);
+            var memoryLayout = getMemoryLayout(f.rawField().getType());
+            memoryLayouts[i] = memoryLayout.withName(f.name());
         }
 
         this.LAYOUT = MemoryLayout.structLayout(memoryLayouts);
 
         //3, 创建 varHandle 以便能反向将 内存中的值读取出来
         for (var f : fields) {
-            var x = LAYOUT.varHandle(MemoryLayout.PathElement.groupElement(f.getName()));
+            var x = LAYOUT.varHandle(MemoryLayout.PathElement.groupElement(f.name()));
             var ff = MethodHandles.insertCoordinates(x, 1, 0L);
             fieldMap.put(f, ff);
         }
