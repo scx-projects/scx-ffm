07643dc8f2eef261e1189f3dfb7886d0214c6f19
scx567888 <scx567888@outlook.com>
2025-07-11 14:21:41 +0800

    dev

--- src/main/java/cool/scx/ffm/mapper/array/IntArrayMapper.java
+++ src/main/java/cool/scx/ffm/mapper/array/IntArrayMapper.java
@@ -1,4 +1,6 @@
-package cool.scx.ffm.mapper;
+package cool.scx.ffm.mapper.array;
+
+import cool.scx.ffm.mapper.Mapper;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
@@ -31,9 +33,13 @@ public class IntArrayMapper implements Mapper {
     }
 
     @Override
-    public Object fromMemorySegment(MemorySegment memorySegment) {
-        value = memorySegment.toArray(JAVA_INT);
-        return value;
+    public void fromMemorySegment(MemorySegment memorySegment) {
+        var temp = memorySegment.toArray(JAVA_INT);
+        // 这里假设 temp 的长度与原始 value 数组完全一致.
+        // 我们不直接替换 value 引用, 是为了保留外部传入的数组对象不变,
+        // 这样外部对原数组的引用依然有效, 相当于模拟 C 中通过指针修改数组的效果.
+        // 因此这里使用 System.arraycopy, 将数据写回原数组.
+        System.arraycopy(temp, 0, value, 0, temp.length);
     }
 
 }
